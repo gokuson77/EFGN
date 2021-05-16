33159a3eae26da413ee8ef8643d64b114719340e
gokuson77 <45629408+gokuson77@users.noreply.github.com>
2021-05-16 20:11:38 +0800

    Delete EFGN directory

--- EFGN/solvers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .SRSolver import SRSolver
-
-def create_solver(opt):
-    if opt['mode'] == 'sr':
-        solver = SRSolver(opt)
-    else:
-        raise NotImplementedError
-
-    return solver
